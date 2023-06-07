3e69ee6e4b36d413dff74d3146fa3f86eb992aa5
Isl0m17 <129589552+Isl0m17@users.noreply.github.com>
2023-06-07 14:50:34 +0300

    Add files via upload

---
+++ lesson_8/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// let ar = [1, 2, 3, 5, 8, 13]; 
+// for(let i = 0; i < ar.length ; i++){
+//     ar[i] *= 2; 
+// }
+// console.log(ar); 
+
+let arrayOfNumber = [1, 2, 3, 4, 5, 6]
+arrayOfNumber.push(17)
+arrayOfNumber.pop()
+delete[0]
+console.log(arrayOfNumber)
